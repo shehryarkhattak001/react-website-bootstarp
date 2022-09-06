9433cc52f43eea182a4763f850a650ef7226e4ff
unknown <shehryar.khansk123@gmail.com>
2022-09-06 21:25:13 +0500

    Footer, Navigation, Services

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -41,7 +41,7 @@ function Footer() {
         </div>
     </div>
 
-    <div className="footer-copyright text-center py-3">© 2020 Copyright:
+    <div className="footer-copyright text-center py-3">© 2022 Copyright:
         
     </div>
 
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Services = () =>{
     return(
         <div>
-            <h1 className='text-center mt-10 my-10'>Services..</h1>
+           
         </div>
         
     )
