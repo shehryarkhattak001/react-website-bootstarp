37edea9d8447187a1d35e938b867c0b874b9b001
unknown <shehryar.khansk123@gmail.com>
2022-09-06 22:20:31 +0500

    refactor code of App, About, Footer, Home, Navigation, Privacyandpolicy, Services

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,106 +1,34 @@
 import React from 'react';
 import Banner from './Banner';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import About from './About';
 import Contact from './Contact';
 import Footer from './Footer';
-import {Link} from "react-router-dom";
-import {Nav} from 'react-bootstrap';
+import Services from './Services';
 
-
-const Home = () =>{
-    return(
-<div>
-
-        <div className='bg-light border-top'>
-            <Banner />
-
-            <div className='container my-4'>
-                <div className='row'>
-                    <h1 className='text-center'>Services</h1>
-
-                </div>
-            </div>
-
-            <div className='container my-4'>
-                       
-                <div className='row'>
-                    <div className='col-md-4' >
-                    <Card>
-                      <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                      <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                          Some quick example text to build on the card title and make up the
-                          bulk of the card's content.
-                        </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-                      </Card.Body>
-                    </Card>
-                    
-                        </div>
-                        <div className='col-md-4' >
-                        <Card>
-                           <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                           <Card.Body>
-                             <Card.Title>Card Title</Card.Title>
-                             <Card.Text>
-                               Some quick example text to build on the card title and make up the
-                               bulk of the card's content.
-                             </Card.Text>
-                             <Button variant="primary">Go somewhere</Button>
-                           </Card.Body>
-                         </Card>
-                        </div>
-                        <div className='col-md-4' >
-                        <Card >
-                        <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                         </Card.Text>
-                           <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                        </Card>
-                        </div>
-                    </div>
-                </div>
-
-          
-
-
-            <div className='container my-4 col-sm-1'>
-                <div className='row'>
-                <button className='btn btn-secondary' type='button'><Nav.Link as={Link} to="/services">Services</Nav.Link></button>
-
-                </div>
-            </div>
-
-            
-            </div>
-
-            <About />
-            <Contact />
-            <Footer />
-            </div>
-
-           
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Services />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
 
 
 
 
 
 
-      
 
-            
-       
 
-            
-                   
-    )
+
+
+
+
+
+
+
+  )
 }
 export default Home
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,10 +1,66 @@
 import React from 'react';
-const Services = () =>{
-    return(
-        <div>
-           
+import { Card, Button, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+const Services = () => {
+    return (
+
+        <div className='bg-light border-top'>
+            <div className='container my-4'>
+                <div className='row'>
+                    <h1 className='text-center'>Services</h1>
+                </div>
+            </div>
+            <div className='container my-4'>
+                <div className='row'>
+                    <div className='col-md-4' >
+                        <Card>
+                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+
+                    </div>
+                    <div className='col-md-4' >
+                        <Card>
+                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className='col-md-4' >
+                        <Card >
+                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+            <div className='container my-4 col-sm-1'>
+                <div className='row'>
+                    <button className='btn btn-secondary' type='button'><Nav.Link as={Link} to="/services">Services</Nav.Link></button>
+                </div>
+            </div>
         </div>
-        
+
     )
 }
 export default Services
