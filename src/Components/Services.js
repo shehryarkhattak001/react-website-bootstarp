fc7c269721965935771a5cdf150173bf43a3ef94
unknown <shehryar.khansk123@gmail.com>
2022-09-09 19:35:03 +0500

    App, About, Cards, FAQ, Footer, Home, Privacyndpolicy, Services, ServicesPage

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Card, Button, Nav } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Cards from './Cards';
+
 const Services = () => {
     return (
 
@@ -8,151 +10,30 @@ const Services = () => {
             <div className='container my-4'>
                 <div className='row'>
                     <h1 className='text-center'>Services</h1>
+                    <p className='text-xl-center '>This is the Services</p>
                 </div>
             </div>
 
             <div className='container my-4'>
-                <div className='row'>
-                    <div className='col-md-3' >
-                        <Card>
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
+                <Cards />
 
-                    </div>
-                    <div className='col-md-3' >
-                        <Card>
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className='col-md-3' >
-                        <Card >
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
+            </div>
 
-                    <div className='col-md-3' >
-                        <Card >
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
+
+
+            <div className='mt-4 px-4 my-3'>
+                <div className='container col-sm-1'>
+                    <div className='row'>
+                        <button className='btn btn-secondary' type='button'><Nav.Link as={Link} to="/servicesPage">Services</Nav.Link></button>
                     </div>
                 </div>
             </div>
 
 
 
-
-            <div className='container my-4'>
-                <div className='row'>
-                    <div className='col-md-3' >
-                        <Card>
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-
-                    </div>
-                    <div className='col-md-3' >
-                        <Card>
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className='col-md-3' >
-                        <Card >
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-
-                    <div className='col-md-3' >
-                        <Card >
-                            <Card.Img variant="top" src="https://picsum.photos/200/150" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <div className='container my-4 col-sm-1'>
-                <div className='row'>
-                    <button className='btn btn-secondary' type='button'><Nav.Link as={Link} to="/services">Services</Nav.Link></button>
-                </div>
-            </div>
 
         </div>
+
 
     )
 }
---
+++ src/Components/ServicesPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Cards from './Cards';
+import Footer from './Footer';
+
+const ServicesPage = () => {
+    return (
+        <>
+            <div className='container'>
+                <Cards />
+                <Cards />
+            </div>
+            <Footer />
+
+        </>
+    )
+}
+export default ServicesPage
